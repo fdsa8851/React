78f2b6e7f7f369cfe63d35abb63eea7eaf9b6589
donghyun <fdsa8851@naver.com>
2023-11-28 14:45:33 +0900

    ccc

--- src/member/Board.js
+++ src/member/Board.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
-import picture from "./../picture/picture1.jpg"
+import picture from "./src/picture/picture1.jpg"
+
 export default class SimpleSlider extends Component {
+
   render() {
+
+    const IMG_FILE = process.env.REACT_APP_IMG_FILE;
+
+    console.log(IMG_FILE);
+
     const settings = {
       dots: true,
       infinite: true,
@@ -10,12 +17,20 @@ export default class SimpleSlider extends Component {
       slidesToShow: 5,
       slidesToScroll: 2
     };
+    
+    
+
     return (
       <div>
         <h2> Single Item</h2>
         <Slider {...settings}>
           <div>
-            <img src={'./../picture/picture1.jpg'} alt="사진"></img>
+          <img
+            src={'${IMG_FILE}'/picture2.jpg} alt="사진">       
+          </img>
+          <img
+            src={picture} alt="사진">       
+          </img>
           </div>
           <div>
             <img src='./../picture/picture2.jpg' alt="사진"></img>
